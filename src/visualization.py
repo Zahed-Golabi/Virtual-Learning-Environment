13c5822cd51e5621529272d6cfa96e1c9308b45b
Zahed Golabi <golabizahed@gmail.com>
2023-11-24 15:34:17 +0330

    implementation of plot_distribution function

--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import time
+import plotly.express as px
+
 
 
 
@@ -26,28 +27,54 @@ class Visualization():
         # Check the distribution of the target feature
         display("targe:", self.df["label"].value_counts())
 
-        # Check the data types of the columns
-        display("Data Types", self.df.dtypes)
-
         # Check for missing values
         display("Count of missing values", self.df.isnull().sum())
 
         # Check summary statistics of the numerical columns
         display("Overall Statistics", self.df.describe())
 
-    def feature_distribution(self, feature_name):
-        """
-        To plot a feature distribution
-        """
-
-        # Visualize the distribution of a feature
-        sns.histplot(data=self.df, x=feature_name, hue="label", kde=True)
-        plt.title("Histogram of " + feature_name)
-        plt.tight_layout()
-        plt.savefig('charts/eda/feature_distribution/{}.png'.format(feature_name))
-        plt.show()
+    def distribution_plot(self, feature, mode="bar"):
+    """
+    plot distribution for each feature
+    """
+    
+    convert = {"code_module":"Modules", "code_presentation":"Semesters",
+              "gender":"Gender", "region":"Region", "highest_education":"Degree",
+              "imd_band":"Imd_band", "age_band":"Age_band", "disability":"Disability"}
+    
+    data = self.df.groupby(x).agg({"id_student":"count"})
+    data = data.reset_index().rename(columns={x:convert[x], "id_student":"Count"})
+    data["Percentage"] = data.apply(lambda row: round(100 *(row["Count"]/data["Count"].sum()),2), axis=1)
+    
+    if mode=="bar":
+        fig = px.bar(data, x=convert[x], y="Count", text="Percentage", color=convert[x],
+                    hover_data=["Count"], template="seaborn")
+        fig.update_layout(margin=dict(l=20, r=50, t=50, b=50),
+                     title="Distribution of: " + x,
+                     xaxis_title=convert[x],
+                     yaxis_title="Count",
+                     legend_title=convert[x],
+                     width=600,
+                     height=400,
+                     uniformtext_minsize=10,
+                     uniformtext_mode="hide")
+    
+    elif mode=="pie":
+        fig = px.pie(data, values="Percentage", names=convert[x], title="Distribution of: " + x)
+        fig.update_traces(textposition='inside', textinfo='percent+label')
         
-
+    else:
+        print("Invalid mode: \n mode is 'bar' or 'pie' ")
+        exit()
+        
+    
+    # save the figure
+    fig.write_image(f"charts/eda/feature_distribution/{x}_{mode}.jpeg")
+    
+    
+    
+    
+    
     def feature_correlation_scatterplot(self, feature_name_x, feature_name_y):
         """
         To plot correlation of two numberical features
